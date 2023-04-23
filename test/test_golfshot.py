eb31b212b65e1a86ff1f1ae4930939cd3955b9ce
VonSquiggles <andrew.vangerpen@icloud.com>
2023-04-22 21:42:47 -0500

    add comments in tests

--- test/test_golfshot.py
+++ test/test_golfshot.py
@@ -4,5 +4,9 @@ import pandas
 def test_sim_out():
     sim = GolfShot(launch_velocity=75,launch_angle=10,rotation_rate=300)
     state_history = sim.run_sim()
+
+    # verify the state history type is a pandas dataframe
     assert isinstance(state_history, pandas.core.frame.DataFrame)
+
+    # verify the state history has 3 columns (x, y, time)
     assert state_history.shape[1] == 3
